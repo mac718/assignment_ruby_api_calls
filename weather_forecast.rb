87eda198d8d1422aac3a960f4a3adf44002b09e2
mac718 <mac718@gmail.com>
2017-11-18 16:31:44 -0500

    more methods, added attr_readers, seperated public and private methods

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -8,22 +8,12 @@ class WeatherForecast
 
   API_KEY = ENV['API_KEY']
 
+  attr_reader :location, :number_of_days, :results 
+
   def initialize(location = 4984247, number_of_days = 5)
     @location = location
-    @number_of_days = number_of_days
+    @number_of_days = number_of_days > 5 ? 5 : number_of_days
     @results = {}
-  end
-
-  def raw_response
-    #16 day seems to now require a paid account; this is for 5 day / 3 hour, which works free of charge
-    @results = HTTParty.get("#{BASE_URI}data/2.5/forecast?id=#{@location}&units=imperial&cnt=#{@number_of_days}&APPID=#{API_KEY}")
-    save_results(@results)
-  end
-
-  def hi_temps
-    parse_results['list'].map do |day|
-      day['main']['temp_max']
-    end
   end
 
   def display_hi_temps
@@ -54,6 +44,20 @@ class WeatherForecast
     puts "Clouds/Precipitation: #{tomorrow[:weather_description]}"
     puts "humidity: #{tomorrow[:humidity]}%"
     puts "Wind Speed: #{tomorrow[:wind_speed]}"
+  end
+
+  private
+
+  def raw_response
+    #16 day forecast seems to now require a paid account; this is for 5 day / 3 hour, which works free of charge
+    @results = HTTParty.get("#{BASE_URI}data/2.5/forecast?id=#{@location}&units=imperial&cnt=#{@number_of_days}&APPID=#{API_KEY}")
+    save_results(results)
+  end
+
+  def hi_temps
+    parse_results['list'].map do |day|
+      day['main']['temp_max']
+    end
   end
 
   def lo_temps
@@ -107,4 +111,4 @@ class WeatherForecast
   end
 end
 
-WeatherForecast.new.display_tomorrows_forecast
+w = WeatherForecast.new
