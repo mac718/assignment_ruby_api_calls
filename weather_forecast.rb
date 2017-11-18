b9545977998a13e8a55dbac38429efda809d060f
mac718 <mac718@gmail.com>
2017-11-18 15:29:29 -0500

    added several convenience methods

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -4,7 +4,7 @@ require 'pry'
 require 'json'
 
 class WeatherForecast
-  BASE_URI = "http://api.openweathermap.org/data/2.5/forecast/daily?"
+  BASE_URI = "http://api.openweathermap.org/"
 
   API_KEY = ENV['API_KEY']
 
@@ -15,23 +15,85 @@ class WeatherForecast
   end
 
   def raw_response
-    @results = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?id=#{@location}&units=imperial&cnt=#{@number_of_days}&APPID=#{API_KEY}")
+    #16 day seems to now require a paid account; this is for 5 day / 3 hour, which works free of charge
+    @results = HTTParty.get("#{BASE_URI}data/2.5/forecast?id=#{@location}&units=imperial&cnt=#{@number_of_days}&APPID=#{API_KEY}")
     save_results(@results)
   end
 
   def hi_temps
-    @results.values[3].map do |day|
-      day[:main][:temp_max]
+    parse_results['list'].map do |day|
+      day['main']['temp_max']
     end
   end
 
+  def display_hi_temps
+    puts "Daliy high temperatures for the next #{@number_of_days} days"
+    hi_temps.each_with_index { |temp, index| puts "#{dates[index]}: #{temp}" }
+  end
+
+  def display_lo_temps
+    puts "Daliy low temperatures for the next #{@number_of_days} days"
+    lo_temps.each_with_index { |temp, index| puts "#{dates[index]}: #{temp}" }
+  end
+
+  def display_todays_forecast
+    puts "Today's forecast:"
+    puts
+    puts "High Temp: #{today[:high_temp]} degrees"
+    puts "Low Temp: #{today[:low_temp]} degrees"
+    puts "Clouds/Precipitation: #{today[:weather_description]}"
+    puts "humidity: #{today[:humidity]}%"
+    puts "Wind Speed: #{today[:wind_speed]}"
+  end
+
+  def display_tomorrows_forecast
+    puts "Today's forecast:"
+    puts
+    puts "High Temp: #{tomorrow[:high_temp]} degrees"
+    puts "Low Temp: #{tomorrow[:low_temp]} degrees"
+    puts "Clouds/Precipitation: #{tomorrow[:weather_description]}"
+    puts "humidity: #{tomorrow[:humidity]}%"
+    puts "Wind Speed: #{tomorrow[:wind_speed]}"
+  end
+
   def lo_temps
+    parse_results['list'].map do |day|
+      day['main']['temp_min']
+    end
+  end
+
+  def dates
+    parse_results['list'].map do |day|
+      day['dt_txt'].split(' ')[0]
+    end
+  end
+
+  def weather_descriptions
+    parse_results['list'].map do |day|
+      day['weather'][0]['description']
+    end
+  end
+
+  def humidity
+    parse_results['list'].map do |day|
+      day['main']['humidity']
+    end
+  end
+
+  def wind_speed
+    parse_results['list'].map do |day|
+      day['wind']['speed']
+    end
   end
 
   def today
+    { high_temp: hi_temps[0], low_temp: lo_temps[0], weather_description: weather_descriptions[0], 
+      humidity: humidity[0], wind_speed: wind_speed[0]  }
   end
 
   def tomorrow
+    { high_temp: hi_temps[1], low_temp: lo_temps[1], weather_description: weather_descriptions[1], 
+      humidity: humidity[1], wind_speed: wind_speed[1]  }
   end
 
    def save_results(response)
@@ -39,6 +101,10 @@ class WeatherForecast
       f.write(response)
     end
   end
+
+  def parse_results
+    JSON.parse(File.read('results.json'))
+  end
 end
 
-WeatherForecast.new.raw_response
+WeatherForecast.new.display_tomorrows_forecast
